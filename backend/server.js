28a8a1bf216e2753f4a9ef5ef0180281577aefbe
Mariam Mousa <eladdad2000@gmail.com>
2024-03-24 22:12:33 +0200

    Added data models

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import products from './data/products.js';
+import connectDB from './config/db.js';
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
