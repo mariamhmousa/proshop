fb5820962f86fb7f38629e41e0eb234851a0ada6
Mariam Mousa <eladdad2000@gmail.com>
2024-04-23 21:33:19 +0200

    Added custom error handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB();
 
@@ -15,5 +16,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
