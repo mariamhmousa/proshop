4adbe1bef3d5001509839a2d19a549490eb98cf7
Mariam Mousa <eladdad2000@gmail.com>
2024-05-11 11:31:02 +0300

    Added CartScreen

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -3,27 +3,25 @@ export const addDecimals = (num) => {
 };
 
 export const updateCart = (state) => {
-          //Calculate items price
-          state.itemsPrice = addDecimals(
-            state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-          );
-    
-          //Calculate shipping price, if items price is above 100$ then 0, else 10$
-          state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-    
-          //Calculate tax price 15%
-          state.taxPrice = addDecimals(
-            Number((0.15 * state.itemsPrice).toFixed(2))
-          );
-    
-          //Calculate total price
-          state.totalPrice = (
-            Number(state.itemsPrice) +
-            Number(state.shippingPrice) +
-            Number(state.taxPrice)
-          ).toFixed(2);
-    
-          //Save to local storage
-          localStorage.setItem('cart', JSON.stringify(state));
-          return state;
-}
+  //Calculate items price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+
+  //Calculate shipping price, if items price is above 100$ then 0, else 10$
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+  //Calculate tax price 15%
+  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+  //Calculate total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+
+  //Save to local storage
+  localStorage.setItem("cart", JSON.stringify(state));
+  return state;
+};
