bbcaf520adf092db9f465e528d35647c8afcd425
Mariam Mousa <eladdad2000@gmail.com>
2024-05-14 19:19:32 +0300

    Added users routes and controller

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUsers,
+    getUserProfile,
+    getUserById,
+    updateUser,
+    updateUserProfile,
+    deleteUser
+ } from "../controllers/userController.js";
+const router = express.Router();
+
+router.route('/').post(registerUser).get(getUsers);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/:id').get(getUserById).delete(deleteUser).put(updateUser);
+export default router;
