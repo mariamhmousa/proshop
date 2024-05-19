7480049e28f8f30bd8dbfd5589f144666872c534
Mariam Mousa <eladdad2000@gmail.com>
2024-05-19 19:10:34 +0300

    Added logout & register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,103 +1,122 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from 'jsonwebtoken';
+import generateToken from "../utils/generateToken.js";
 
 // @desc Auth user & get token
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
-            expiresIn: '30d'
-        });
-
-        //Set jwt as http only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000 //30 Days in millisecond
-        })
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-        });
-    } else {
-        res.status(401);
-        throw new Error("Invalid email or password");
-    }
-
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc Register user
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('register user');
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Logout user / clear cookie
 // @route POSt /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expiresIn: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully." });
 });
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+  res.send("get user profile");
 });
 
 // @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('update user profile');
+  res.send("update user profile");
 });
 
 // @desc Get users
 // @route GET /api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-    res.send('get users');
+  res.send("get users");
 });
 
 // @desc Get user
 // @route GET /api/users/:id
 // @access Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-    res.send('get user by id');
+  res.send("get user by id");
 });
 
 // @desc Delete user
 // @route DELETE /api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-    res.send('delete user');
+  res.send("delete user");
 });
 
 // @desc Update user
 // @route PUT /api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send('update user');
+  res.send("update user");
 });
 
 export {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUsers,
-    getUserProfile,
-    getUserById,
-    updateUser,
-    updateUserProfile,
-    deleteUser
-}
+  authUser,
+  registerUser,
+  logoutUser,
+  getUsers,
+  getUserProfile,
+  getUserById,
+  updateUser,
+  updateUserProfile,
+  deleteUser,
+};
