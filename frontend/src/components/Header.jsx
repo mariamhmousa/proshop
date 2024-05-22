ed0ab59b8fb48b1b3456ab82566c844c2e7a330c
Mariam Mousa <eladdad2000@gmail.com>
2024-05-22 19:40:10 +0300

    Added the login functionality in the UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import { useSelector } from "react-redux";
@@ -7,6 +7,13 @@ import logo from "../assets/knitting.png";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    console.log('logout');
+  }
+
   console.log(cartItems);
 
   return (
@@ -33,11 +40,23 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
               <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </LinkContainer>
+
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,50 +1,96 @@
-import { useState } from "react"
-import { Link } from 'react-router-dom';
-import { Form, Row, Col, Button, FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Form,
+  Row,
+  Col,
+  Button,
+  FormGroup,
+  FormLabel,
+  FormControl,
+} from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        console.log('submit');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
     }
+  }, [userInfo, redirect, navigate]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect);
+    } catch (err) {
+        toast.error(err?.data?.message || err.error);
+    }
+  };
   return (
     <FormContainer>
-        <h1>Sign In</h1>
-        <Form onSubmit={submitHandler}>
-            <FormGroup controlId="email" className="my-3">
-                <FormLabel>Email Address</FormLabel>
-                <FormControl
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                ></FormControl>
-            </FormGroup>
-            <FormGroup controlId="password" className="my-3">
-                <FormLabel>Password</FormLabel>
-                <FormControl
-                    type="password"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                ></FormControl>
-            </FormGroup>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <FormGroup controlId="email" className="my-3">
+          <FormLabel>Email Address</FormLabel>
+          <FormControl
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></FormControl>
+        </FormGroup>
+        <FormGroup controlId="password" className="my-3">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></FormControl>
+        </FormGroup>
 
-            <Button type="submit" variant="primary" className="mt-2">
-                Sign In
-            </Button>
-        </Form>
-        <Row className="py-3">
-            <Col>
-            New Customer? <Link to='/register'>Register</Link>
-            </Col>
-        </Row>
+        <Button
+          type="submit"
+          variant="primary"
+          className="mt-2"
+          disabled={isLoading}
+        >
+          Sign In
+        </Button>
+
+        {isLoading && <Loader />}
+      </Form>
+      <Row className="py-3">
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register/redirect?=${redirect}` : "/register"}>
+            Register
+          </Link>
+        </Col>
+      </Row>
     </FormContainer>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
